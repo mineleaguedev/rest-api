14f55d1b6d56ab09bde4c61ada1b9ea740dce852
Abdymazhit <islam.abdymazhit@gmail.com>
2022-02-02 21:11:31 +0300

    Добавлено подключение к базе данных

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
@@ -11,6 +15,16 @@ func main() {
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
+
+	if _, err := sql.Open("mysql", fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		os.Getenv("db.username"),
+		os.Getenv("db.password"),
+		os.Getenv("db.host"),
+		os.Getenv("db.port"),
+		os.Getenv("db.dbname")),
+	); err != nil {
+		log.Fatalf("Error connecting to database: %s", err)
 	}
 
 	if err := router.Run(":8080"); err != nil {
