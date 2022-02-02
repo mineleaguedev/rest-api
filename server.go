957f5ff1c599da1281b99ea9089669e4413834f5
Abdymazhit <islam.abdymazhit@gmail.com>
2022-02-02 21:06:30 +0300

    Добавлены переменные среды

--- server.go
+++ server.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
 	router := gin.Default()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading env variables: %s", err.Error())
+	}
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatalf("Error starting server: %s", err)
