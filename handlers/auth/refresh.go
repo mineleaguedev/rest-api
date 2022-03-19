22f1378b4f9b4b63808997f416b4504fce5a4b77
Abdymazhit <islam.abdymazhit@gmail.com>
2022-03-19 22:37:58 +0300

    Метод обновления токена добавлен в обработчик auth

--- handlers/auth/refresh.go
+++ handlers/auth/refresh.go
@@ -1,4 +1,4 @@
-package general
+package auth
 
 import (
 	"fmt"
@@ -9,7 +9,7 @@ import (
 	"strconv"
 )
 
-func RefreshHandler(c *gin.Context) {
+func (h *Handler) RefreshHandler(c *gin.Context) {
 	mapToken := map[string]string{}
 	if err := c.ShouldBindJSON(&mapToken); err != nil {
 		c.JSON(http.StatusUnprocessableEntity, errors.ErrMissingRefreshToken)
@@ -21,49 +21,49 @@ func RefreshHandler(c *gin.Context) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
-		return Middleware.RefreshTokenKey, nil
+		return h.middleware.RefreshTokenKey, nil
 	})
 	if err != nil {
-		Service.HandleErr(c, http.StatusUnauthorized, errors.ErrInvalidRefreshToken)
+		h.services.HandleErr(c, http.StatusUnauthorized, errors.ErrInvalidRefreshToken)
 		return
 	}
 
 	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
-		Service.HandleErr(c, http.StatusUnauthorized, errors.ErrExpiredRefreshToken)
+		h.services.HandleErr(c, http.StatusUnauthorized, errors.ErrExpiredRefreshToken)
 		return
 	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok || !token.Valid {
-		Service.HandleErr(c, http.StatusUnauthorized, errors.ErrExpiredRefreshToken)
+		h.services.HandleErr(c, http.StatusUnauthorized, errors.ErrExpiredRefreshToken)
 		return
 	}
 
 	refreshUuid, ok := claims["refresh_uuid"].(string)
 	if !ok {
-		Service.HandleErr(c, http.StatusUnprocessableEntity, errors.ErrRefreshTokenUuidNotExists)
+		h.services.HandleErr(c, http.StatusUnprocessableEntity, errors.ErrRefreshTokenUuidNotExists)
 		return
 	}
 
 	userId, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["id"]), 10, 64)
 	if err != nil {
-		Service.HandleErr(c, http.StatusUnprocessableEntity, errors.ErrRefreshTokenUserIdNotExists)
+		h.services.HandleErr(c, http.StatusUnprocessableEntity, errors.ErrRefreshTokenUserIdNotExists)
 		return
 	}
 
-	td, err := Service.CreateToken(userId)
+	td, err := h.services.CreateToken(userId)
 	if err != nil {
-		Service.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrFailedTokenCreation, err)
+		h.services.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrFailedTokenCreation, err)
 		return
 	}
 
-	if deleted, err := Service.DeleteSession(refreshUuid); err != nil || deleted == 0 {
-		Service.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrDeletingSession, err)
+	if deleted, err := h.services.DeleteSession(refreshUuid); err != nil || deleted == 0 {
+		h.services.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrDeletingSession, err)
 		return
 	}
 
-	if err := Service.SaveAuthSession(userId, td); err != nil {
-		Service.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrSavingAuthSession, err)
+	if err := h.services.SaveAuthSession(userId, td); err != nil {
+		h.services.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrSavingAuthSession, err)
 		return
 	}
 
