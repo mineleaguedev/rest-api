7a6ea980691ba6e1fd6c2b0dd0935401fa3a3b3d
Abdymazhit <islam.abdymazhit@gmail.com>
2022-03-19 22:40:25 +0300

    Метод выхода с аккаунта добавлен в обработчик auth

---
+++ handlers/auth/logout.go
@@ -0,0 +1,25 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mineleaguedev/rest-api/errors"
+	"github.com/mineleaguedev/rest-api/models"
+	"net/http"
+)
+
+func (h *Handler) LogoutHandler(c *gin.Context) {
+	accessDetails, err := h.services.ExtractTokenMetadata(c.Request)
+	if err != nil {
+		h.services.HandleErr(c, http.StatusUnauthorized, err)
+		return
+	}
+
+	if deleted, err := h.services.DeleteSession(accessDetails.AccessUuid); err != nil || deleted == 0 {
+		h.services.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrDeletingSession, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, models.Response{
+		Success: true,
+	})
+}
