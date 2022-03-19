8b5101263339538dc75c2e83711504dce8de3394
Abdymazhit <islam.abdymazhit@gmail.com>
2022-03-19 22:41:49 +0300

    Метод middleware добавлен в обработчик auth

---
+++ handlers/auth/middleware.go
@@ -0,0 +1,29 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mineleaguedev/rest-api/errors"
+	"net/http"
+)
+
+func (h *Handler) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		accessDetails, err := h.services.ExtractTokenMetadata(c.Request)
+		if err != nil {
+			h.services.HandleErr(c, http.StatusUnauthorized, err)
+			c.Abort()
+			return
+		}
+
+		userId, err := h.services.GetAuthSession(accessDetails)
+		if err != nil {
+			h.services.HandleInternalErr(c, http.StatusInternalServerError, errors.ErrGettingAuthSession, err)
+			c.Abort()
+			return
+		}
+
+		c.Set("userId", userId)
+
+		c.Next()
+	}
+}
